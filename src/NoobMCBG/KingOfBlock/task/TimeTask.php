25f22c279630f6644bc2f7cea435ac0e1d3b43cc
ClickedTran <58399779+Clickedtran@users.noreply.github.com>
2023-12-13 19:46:42 +0700

    Update TimeTask.php

--- src/NoobMCBG/KingOfBlock/task/TimeTask.php
+++ src/NoobMCBG/KingOfBlock/task/TimeTask.php
@@ -21,23 +21,26 @@ class TimeTask extends Task {
           $mode = $this->plugin->getMode();
 	  if(count($all_time->getAll()) >= 1){
 	     foreach($all_time->getAll() as $player => $time){
-		 if($time == 0){
-		    $all_time->remove($player);
-                    $mode->set($player, "off");
-                    $mode->save();                  
-		    if($this->plugin->getServer()->getPlayerByPrefix($player) !== null){
-		       $this->plugin->getServer()->getPlayerByPrefix($player)->sendMessage("§9[§b KingOfBlock §9] §e Your usage period has ended!");
-                       $ranks->unsetPlayerPermission($this->plugin->getServer()->getPlayerByPrefix($player), "kingofblock.command.use");
-                       break;
-                    }		          
-                    continue;
-		 }
-                 if($this->plugin->getServer()->getPlayerByPrefix($player) !== null){
-	            $this->plugin->getServer()->getPlayerByPrefix($player)->sendMessage("§9[§b KingOfBlock §9]§a Your remaining usage time is §8$time §aminutes !");
-		 }
-		 $all_time->set($player, $time - 1);
-                 $all_time->save();
-	     }		
+	         if($mode->get($player) == "on"){
+		    if($all_time->get($player) == 0){
+		       $all_time->remove($player);
+                       $mode->set($player, "off");
+                       $mode->save();                  
+		       if($this->plugin->getServer()->getPlayerByPrefix($player) !== null){
+		          $this->plugin->getServer()->getPlayerByPrefix($player)->sendMessage("§9[§b KingOfBlock §9] §e Your usage period has ended!");
+                          $ranks->unsetPlayerPermission($this->plugin->getServer()->getPlayerByPrefix($player), "kingofblock.command.use");
+                          break;
+                       }
+		     $ranks->unsetPlayerPermission($this->plugin->getServer()->getPlayerByPrefix($player), "kingofblock.command.use");
+                     continue;
+		  }
+                  if($this->plugin->getServer()->getPlayerByPrefix($player) !== null){
+	             $this->plugin->getServer()->getPlayerByPrefix($player)->sendMessage("§9[§b KingOfBlock §9]§a Your remaining usage time is §8$time §aminutes !");
+		  }
+		  $all_time->set($player, $time - 1);
+                  $all_time->save();
+	       }
+	     }
 	}
      }
 }
